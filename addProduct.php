f18faecc5ce6d87ece5f63fd71fa644fb2743d4e
Emma Bolger <emma.bolger303@gmail.com>
2026-04-20 11:17:29 +0100

    i made a mistake

--- addProduct.php
+++ addProduct.php
@@ -1,9 +1,17 @@
 <?php
 include("db.php");
 
+// Prevent errors if page is accessed directly
+if (!isset($_POST['name']) || !isset($_POST['price'])) {
+    header("Location: sellerdashboard.html");
+    exit();
+}
+
+// Get form data
 $name = $_POST['name'];
 $price = $_POST['price'];
 
+// Insert into database
 $stmt = $conn->prepare("INSERT INTO products (name, price) VALUES (?,?)");
 $stmt->bind_param("sd", $name, $price);
 $stmt->execute();
